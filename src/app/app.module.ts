62554956271b612b4505d88310dad36bd232612a
tangerine2003 <chrisc03@hotmail.com>
2019-04-07 13:04:26 -0400

    fixed error in module with component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import { DayCardComponent } from './weather/day-card/day-card.component';
 
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent,
